41b5db205b77fc8020cfcbb028f8e05f1eadbd59
Chandan <cchandel@uwo.ca>
2023-09-30 09:53:37 -0400

    nano model pushed

---
+++ machine learning algorithms/code/yolo-models-generator.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+ 
+# Load the model.
+model_name = 'yolov8n'
+model = YOLO(model_name+'.pt')
+# model = YOLO('yolov8s.pt')
+# model = YOLO('yolov8m.pt')
+ 
+# Training.
+results = model.train(
+   data='C:\\Users\\cchan\\computer-vision\\machine learning algorithms\\dataset\\data.yaml',
+   imgsz=640,
+   epochs=1,
+   batch=8,
+   name='{}_custom'.format(model_name)
+)
+#saving the model
+# Export the model
+model.export(format='onnx')
